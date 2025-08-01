7d300ee1f9d6ff6efa064f3193ba372b9e4417ff
Rafael Gutiérrez <abadon.gutierrez@gmail.com>
2025-07-31 23:33:32 -0600

    simple a2a-client

---
+++ src/test/java/com/jabaddon/learning/a2a/a2aclient/A2aClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jabaddon.learning.a2a.a2aclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class A2aClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
